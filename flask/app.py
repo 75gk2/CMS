2f467fe642e014962bf7ddb5df3487f429cd5e21
75gk2 <kgargula75@tlkrakowpl.onmicrosoft.com>
2022-05-07 17:14:57 +0200

    Dekoduje z pliku JSON w svelte-app/src/data.json. Grafiki w svelte-app/public/gfx. Teraz musimy napisać formularz który będzie generował JSONa o takiej strukturze jak jest w data.json

--- flask/app.py
+++ flask/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form
+
+class MenuForm(Form):
+    menuRadio = RadioField('Menu', choices=[('ver','vertical'),('hor','horizontal')])
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    return render_template("index.html", form=MenuForm(), adr = "menu")
+
+@app.route('/menu', methods=["GET", "POST"])
+def editDataByAdmin():
+    login = request.form['login']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
