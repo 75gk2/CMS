46ceffe53ee0087d462ca7c51100d1c44511e6f5
75gk2 <kgargula75@tlkrakowpl.onmicrosoft.com>
2022-06-13 23:12:48 +0200

    demo

--- svelte-app/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    './public/**/*.{html,js}',
-    './src/**/*.{html,js,svelte}',
-    './node_modules/tw-elements/dist/js/**/*.js'],
-  plugins: [
-    require('tw-elements/dist/plugin')
-  ]
-}
